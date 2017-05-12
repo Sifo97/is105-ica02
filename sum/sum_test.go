493adda6c9dd9d6da10898d6e3e1f9f0f9843d53
Sifo97 <sindrf16@student.uia.no>
2017-05-12 17:15:02 +0200

    Lagde repository

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -20,3 +20,79 @@ func TestSumInt8(t *testing.T) {
 		}
 	}
 }
+
+var sum_tests_uint32 = []struct {
+	n1		 uint32
+	n2		 uint32
+	expected uint32
+}{
+	{1, 2, 3},
+	{4, 5, 9},
+	{120, 1, 119},
+}
+
+func TestSumUint32(t *testing.T) {
+	for _, v := range sum_tests_uint32 {
+		if val := SumUint32(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+			
+		}
+	}
+}
+
+var sum_tests_int64 = []struct {
+	n1		 int64
+	n2		 int64
+	expected int64
+}{
+	{1, 2, 3},
+	{4, 5, 9},
+	{120, 1, 119},
+}
+
+func TestSumInt64(t *testing.T) {
+	for _, v := range sum_tests_int64 {
+		if val := SumInt64(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+			
+		}
+	}
+}
+
+var sum_tests_int32 = []struct {
+	n1		 int32
+	n2		 int32
+	expected int32
+}{
+	{1, 2, 3},
+	{4, 5, 9},
+	{120, 1, 119},
+}
+
+func TestSumInt32(t *testing.T) {
+	for _, v := range sum_tests_int32 {
+		if val := SumInt32(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+			
+		}
+	}
+}
+
+var sum_tests_float64 = []struct {
+	n1		 float64
+	n2		 float64
+	expected float64
+}{
+	{1.1, 2.2, 3.3},
+	{4.1, 5.2, 9.3},
+	{120.1, 1.2, 119.3},
+}
+
+func TestSumFloat64(t *testing.T) {
+	for _, v := range sum_tests_float64 {
+		if val := SumFloat64(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%f, %f) returned %f, expected %f", v.n1, v.n2, val, v.expected)
+			
+		}
+	}
+}
