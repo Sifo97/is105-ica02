fbcb67b0295191e2754ae4ac2ff68ee1ab2a0fd5
Sifo97 <sindrf16@student.uia.no>
2017-05-13 16:16:22 +0200

    La til Benchmark tester

--- main_sort.go
+++ main_sort.go
@@ -4,9 +4,9 @@ import (
 	"./algorithms"
 )
 func main() {
-fmt.Println("Trying bouble sorting...")
-aList := []int{8,4,2,7,1,3,5}
-fmt.Println("Original: ", aList)
-newList := algorithms.Bubble_sort_modified(aList)
-fmt.Println("Sorted: ", newList)
+	fmt.Println("Trying bouble sorting...")
+	aList := []int{8,4,2,7,1,3,5}
+	fmt.Println("Original: ", aList)
+	newList := algorithms.Bubble_sort_modified(aList)
+	fmt.Println("Sorted: ", newList)
 }
